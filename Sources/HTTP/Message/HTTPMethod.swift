71f1637cefc95998ee62ac0946e80c46277a672e
Dan North <daniel_north@pretmail.net>
2023-06-19 20:59:52 -0600

    PUT method

--- Sources/HTTP/Message/HTTPMethod.swift
+++ Sources/HTTP/Message/HTTPMethod.swift
@@ -3,6 +3,7 @@ public struct HTTPMethod: RawRepresentable, Hashable, Sendable {
     public static let get = HTTPMethod(rawValue: "GET")
     public static let post = HTTPMethod(rawValue: "POST")
     public static let patch = HTTPMethod(rawValue: "PATCH")
+    public static let put = HTTPMethod(rawValue: "PUT")
     public static let delete = HTTPMethod(rawValue: "DELETE")
     public static let head = HTTPMethod(rawValue: "HEAD")
     
